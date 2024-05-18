4549838c643043c5f80882e143a2477458c03f70
SATYAJIT6253 <119771949+SATYAJIT6253@users.noreply.github.com>
2024-05-18 12:23:27 +0530

    update the post controller

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {getAllPostcontroller,createpostcontroller,likeunlikeController,updatepostcontroller, deletpostcontroller} = require("../controllers/postController");
 const postmiddleware =  require("../middlewares/requireUser");
-router.get("/all",postmiddleware,getAllPostcontroller);
+router.get("/all",getAllPostcontroller);
 router.post("/",postmiddleware,createpostcontroller);
 router.post("/like",postmiddleware,likeunlikeController);
 router.put("/",postmiddleware,updatepostcontroller);
